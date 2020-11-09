ad71e5db4b820550703ca00419ab8caed51e21b2
Asandei Stefan <52822996+NikolaTesla13@users.noreply.github.com>
2020-11-09 22:17:20 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
 @ECHO off
 
-echo Building routes/routes.go
-go build src/routes/routes.go
-
-echo Installing modules
-go install
-
-echo Running main.go
-go run src/main.go
+go run src/main.go src/routes.go
